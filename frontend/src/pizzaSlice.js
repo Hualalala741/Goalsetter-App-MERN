7665bda386d58c578af153ddd87bc55236f76bb9
Yantong <yantongguo111@gmail.com>
2025-02-05 02:21:45 -0500

    Added frontend UI

---
+++ frontend/src/pizzaSlice.js
@@ -0,0 +1,13 @@
+// import { createSlice } from "@reduxjs/toolkit/dist";
+// const initialState = {
+//   toppings:['pepperoni'],
+//   gluten: true
+// }
+
+// export const pizzaSlice = createSlice({
+//   name:'pizza',
+//   initialState,
+//   reducers:{
+
+//   }, 
+// })
